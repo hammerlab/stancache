e107e39a0d0e24d57627fb7ca983e0a0e62352a9
Jacki Buros <jackinovik@gmail.com>
2016-10-31 13:43:23 +0000

    more verbose message

--- stancache/stancache.py
+++ stancache/stancache.py
@@ -101,7 +101,7 @@ def _cached_stan_fit(model_name='anon_model', file=None, model_code=None,
                                                                pystan=pystan.__version__,
                                                                cython=Cython.__version__))])
     else:
-        logger.info('Note - no model code detected (from file or by code)')
+        logger.info('Note - no model code detected, from file ({}) or by code (len {})'.format(file, len(model_code)))
     if fit_cachefile:
         # if cachefile given, assume cache_only 
         if cache_only is None:
